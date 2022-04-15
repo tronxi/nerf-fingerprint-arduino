0501a5c6aab4b34642786bbdd163467a4aeeb7cb
tronxi <sergio.gsanchez97@gmail.com>
2022-04-15 21:26:20 +0200

    added file

--- nerf-arduino/src/main/java/com/tronxi/automaticgun/infrastructure/serial/FingerprintSerialPort.java
+++ nerf-arduino/src/main/java/com/tronxi/automaticgun/infrastructure/serial/FingerprintSerialPort.java
@@ -4,6 +4,9 @@ import com.fazecast.jSerialComm.SerialPort;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class FingerprintSerialPort {
@@ -14,14 +17,18 @@ public class FingerprintSerialPort {
         this.fingerPrintSerialPort = fingerPrintSerialPort;
     }
 
-    public void enroll() {
+    public void enroll()  {
         try {
             fingerPrintSerialPort.getOutputStream().write("e".getBytes());
             fingerPrintSerialPort.getOutputStream().flush();
-            fingerPrintSerialPort.getOutputStream().write("1".getBytes());
+            String nextPosition = getNextPosition();
+            System.out.println(nextPosition);
+            fingerPrintSerialPort.getOutputStream().write(nextPosition.getBytes());
             fingerPrintSerialPort.getOutputStream().flush();
+            Integer updatedNextPosition = Integer.parseInt(nextPosition) + 1;
+            setNextPosition(updatedNextPosition);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException();
         }
     }
 
@@ -31,6 +38,23 @@ public class FingerprintSerialPort {
             fingerPrintSerialPort.getOutputStream().flush();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private String getNextPosition() {
+        try {
+            return Files.readString(Path.of("position.txt")).replaceAll("\\n", "").replaceAll("\"", "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setNextPosition(Integer position) {
+        String ps = position.toString();
+        try {
+            Files.write(Path.of("position.txt"), ps.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
